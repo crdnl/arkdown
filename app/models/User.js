e06fd163b51054a04ae2493b8801bfc4363a77f2
Quinton <qmarchi@Quintons-iMac.local>
2017-05-12 23:59:24 -0500

    Update User Model and tests

--- app/models/User.js
+++ app/models/User.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 
 const userSchema = new mongoose.Schema({
+	name: { type: String, unique: true },
 	email: { type: String, unique: true },
 
 	password: { type: String, required: true },
