bcaa09213422b0b3f35116261d52de2a3a52d73e
Quinton Marchi <quinton@fractalcore.net>
2017-06-18 19:43:42 -0500

    Because I'm an idiot...
    user is already passed to pug when rendering

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -1,19 +1,6 @@
-const User = require("../models/User");
-
 module.exports.getSettings = (req, res) => {
-	User.findOne({ name: req.user.name }, (userErr, user) => {
-		if (userErr || user == null) {
-			console.log(userErr);
-			console.log(user);
-			req.flash("error", { msg: "Umm... This is Embarassing... We couldn't find you...." });
-			req.flash("error", { msg: "Please contact crdnl on GitHub" });
-			return res.redirect("/");
-		}
-
-		res.render("user/settings", {
-			title: "User Setttings",
-			user
-		});
+	res.render("user/settings", {
+		title: "User Setttings"
 	});
 };
 
