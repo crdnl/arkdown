1d4f43092cb1bd69d78fb65dc315fd149f0fec8e
Quinton Marchi <quinton@fractalcore.net>
2017-07-01 09:42:12 -0500

    Add Profile Image updating

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -8,14 +8,29 @@ module.exports.getDelete = (req, res) => {
 	res.send(200);
 };
 
-module.exports.postInfo = (req, res, next) => {
-	res.send(200);
-	next();
-};
+module.exports.postInfo = (req, res) => {
+	console.log(req);
+	req.assert("profileImage", "Header Image is invalid").matches(/^https?:\/\/(\w+\.)?imgur.com\/(\w*\d\w*)+(\.[a-zA-Z]{3})?$/);
 
-module.exports.postEmail = (req, res, next) => {
-	res.send(200);
-	next();
+	const errors = req.validationErrors();
+
+	if (errors) {
+		req.flash("error", errors);
+		return res.redirect("/user/settings");
+	}
+
+	req.body.profileImage = req.body.profileImage.replace(/^http:\/\//i, "https://");
+
+	req.user.profileImage = req.body.profileImage;
+
+	req.user.save((error) => {
+		if (error) {
+			req.flash("error", { msg: "There was an error updating the profile image." });
+			return res.redirect("/user/settings");
+		}
+		req.flash("success", { msg: "Sick profile picture bro!" });
+		return res.redirect("/user/settings");
+	});
 };
 
 module.exports.postPassword = (req, res, next) => {
